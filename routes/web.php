c388c09e5e3bee7644c4cc954fa8385fd26b20ea
Ermiaskefelegn <biremesfin@gmail.com>
2020-03-11 13:13:55 +0300

    admin and user login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/springboard', function () {
+    return view('springboard');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('admin/login', 'Auth\AdminloginController@showLoginform')->name('admin.login');
+Route::post('admin/login', 'Auth\AdminloginController@login')->name('admin.login.submit');
+Route::get('admin/', 'AdminController@index')->name('admin.dashboard');
